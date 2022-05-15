9b872feaf3aa7e77fb02e9b5c141dd8630677863
wellpinho <wellpinho@outlook.com>
2022-05-15 10:10:14 -0300

    ✨ NEW FEATURE: sheader and button signOut

---
+++ src/components/ui/header/header.tsx
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+import { FiLogOut } from "react-icons/fi";
+import styles from "./header.module.scss";
+
+// para chamar funcionalidade de logout
+import { AuthContext } from "../../../contexts/AuthContext";
+
+export default function Header() {
+  const { user, signOut } = useContext(AuthContext);
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+        <Link href="/dashboard">
+          {/* a tag <img> do html precisa ser a do next como <Img> */}
+          <Image src="/logo.svg" alt="" width={190} height={60} />
+        </Link>
+
+        {/* caso queira exibir boas vindas ao user logado chama o user no useContext
+        <h1>Bem vindo {user?.name}</h1> */}
+
+        <nav>
+          <Link href="/categories">
+            <a>Categoria</a>
+          </Link>
+          <Link href="/menu">
+            <a>Cardápio</a>
+          </Link>
+
+          {/* botão que chama a função signOut no userContext */}
+          <button onClick={signOut}>
+            <FiLogOut color="#fff" size={24} />
+          </button>
+        </nav>
+      </div>
+      <h1>Header</h1>
+    </header>
+  );
+}
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,20 @@
+import Head from "next/head";
+import Header from "../../components/ui/header/header";
 import { canSSRAuth } from "../../utils/canSSRAuth";
 
 export default function Dashboard() {
-  return <h1>Welcome to dashboard</h1>;
+  return (
+    <>
+      <Head>
+        <title>Painel - Love Sushi</title>
+      </Head>
+
+      <div>
+        <Header />
+        <h1>Painel</h1>
+      </div>
+    </>
+  );
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
